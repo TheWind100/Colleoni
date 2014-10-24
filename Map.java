3d75f2350e6f5ee053da35bbef3474a9f04d8dd2
Xystus- <happyface173@earthlink.net>
2014-10-24 12:13:13 -0400

    Added names

--- Map.java
+++ Map.java
@@ -1,10 +1,21 @@
 public class Map
 {
    private Tile[] tiles;
-   private String[] names = {"Venice", "Milan"};
+   private String[] landnames = {"Avingnon","Swiss","Tyrol","Austria","Hungary","Marseille","Provence","Saluzzo","Turin",
+                              "Como","Milan","Carinthia","Slavonia","Savoy","Montferral","Pavia","Milan","Trent",
+                              "Genoa","F???","Parma","Cremona","Bergamo","Trent","Modena","Mantua","Verona"
+                              "Friuli","Carnola","Lucca","Bologna","Ferrara","Padua","Traviso","Pisa","Pistoia"
+                              ,"Florence","Urbino","Piombino","Siena","Arezzo","P???","Perugia","Spoleto","Ancona"
+                              ,"Rome","Capua","Aquila","Naples","Salerno","Bari","Otranio","Sardinia"
+                              ,"Corsica","Tunis","Palermo","Messina","Venice","Istria","Croatia","Dalmatia"
+                              ,"Bosnia","Herzegovina","Ragusa","Albania","Durazzo"};
+   private String[] seanames = {"Gulf of Lions","Ligurian Sea","Tyrrhenian Sea",
+                                 "Western Mediterranean","Central Mediterranean",
+                                 "Gulf of Naples","Ionian Sea","Upper Adriatic",
+                                 "Lower Adriatic"};
    public Map()
    {
-      tiles = new Tile[names.length];
+      tiles = new Tile[landnames.length+seanames.length];
    }
    public void addTile(Tile tile, int k)
    {
